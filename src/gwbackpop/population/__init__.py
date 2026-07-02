28d0cb672501ae1138e307a1a4719b007eecab5c
Ignacio Magana <imhernan@andrew.cmu.edu>
2026-07-01 21:08:44 -0400

    Avoid JAX import in injection setup

---
+++ src/gwbackpop/population/__init__.py
@@ -0,0 +1,5 @@
+"""Population-level helpers and constants."""
+
+from gwbackpop.population.constants import POP_PARAM_NAMES
+
+__all__ = ["POP_PARAM_NAMES"]
---
+++ src/gwbackpop/population/constants.py
@@ -0,0 +1,16 @@
+"""Lightweight population constants with no JAX/NumPyro imports."""
+
+from __future__ import annotations
+
+POP_PARAM_NAMES = [
+    "mu_logalpha1",
+    "sig_logalpha1",
+    "mu_logalpha2",
+    "sig_logalpha2",
+    "a_f1",
+    "b_f1",
+    "a_f2",
+    "b_f2",
+    "sigma_v1",
+    "sigma_v2",
+]
